c5d801e041bef28ba271bc1eab1ecf836b943ffc
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-22 19:29:10 +0530

    final commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  allTodos: (state) => state.todos,
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter((todo) => todo.id !== id)),
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    commit('setTodos', response.data);
+  },
+
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      'https://jsonplaceholder.typicode.com/todos',
+      { title, completed: false }
+    );
+
+    commit('newTodo', response.data);
+  },
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit('removeTodo', id);
+  },
+  async filterTodos({ commit }, e) {
+    console.log(e);
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
